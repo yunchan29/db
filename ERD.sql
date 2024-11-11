377f009e1b389efee77ee1614a1b77040a3e08c0
Pami <114650438+badtzpami@users.noreply.github.com>
2024-11-12 00:15:16 +0800

    Update ERD.sql

--- ERD.sql
+++ ERD.sql
@@ -118,6 +118,48 @@ Table delivery {
   shop_location varchar(100)
 }
 
+Table supplier_material {
+  sm_id int [primary key]
+  material_name varchar(222) [not null]
+  type varchar(222) [not null]
+  stock int [not null]
+  enter_stock int [not null]
+  selling_price decimal(10,2)
+  unit varchar(222) [not null]
+  remarks varchar(222) [not null]
+  comment varchar(222) [not null]
+  image varchar(222) [not null]
+  user_id int [not null]
+  date_created_at datetime [not null]
+  date_updated_at datetime [not null]
+}
+
+Table supplier_history {
+  sh_id int [primary key]
+  material_name varchar(222) [not null]
+  type varchar(222) [not null]
+  stock int [not null]
+  enter_stock int [not null]
+  selling_price decimal(10,2)
+  unit varchar(222) [not null]
+  remarks varchar(222) [not null]
+  user_id int [not null]
+  date_created_at datetime [not null]
+  date_updated_at datetime [not null]
+}
+
+Table cart {
+  ct_id int [primary key]
+  sm_id int [not null]
+  cart_code varchar(222) [not null]
+  sell_price decimal(10,2)
+  quantity int [not null]
+  total int [not null]
+  user_id int [not null]
+  date_created_at datetime [not null]
+  date_updated_at datetime [not null]
+}
+
 Ref: product.ca_id > category.ca_id
 Ref: loginhistory.user_id > users.user_id
 Ref: inventory.ma_id > material.ma_id
@@ -126,3 +168,7 @@ Ref: menu.ma_id > material.ma_id
 Ref: sale.pr_id > product.pr_id
 Ref: supplier.ma_id > material.ma_id
 Ref: delivery.su_id > supplier.su_id
+Ref: cart.sm_id > supplier_material.sm_id
+Ref: cart.user_id > users.user_id
+Ref: supplier_material.user_id > users.user_id
+Ref: supplier_history.user_id > users.user_id
